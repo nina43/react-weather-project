f434b27878d92e87525d16baf03fc298ba0e6d26
nina43 <161338284+nina43@users.noreply.github.com>
2024-09-25 12:26:23 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Martyna Ziarek and is{" "}
-        <a
-          href="https://github.com/nina43/react-weather-project"
-          target="_blank"
-          noopener
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Martyna Ziarek and is{" "}
+          <a
+            href="https://github.com/nina43/react-weather-project"
+            target="_blank"
+            noopener
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
