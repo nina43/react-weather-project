3a0305ce8fb0f7b21a19eff3fb54a4b82c43aed1
nina43 <161338284+nina43@users.noreply.github.com>
2024-09-25 11:51:39 +0100

    added the GitHub repository url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <footer>
+        This project was coded by Martyna Ziarek and is{" "}
+        <a
+          href="https://github.com/nina43/react-weather-project"
+          target="_blank"
+          noopener
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub
+      </footer>
     </div>
   );
 }
