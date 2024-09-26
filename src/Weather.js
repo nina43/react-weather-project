b7d8f404c093732e6c83b84a8793002c44f372bb
nina43 <161338284+nina43@users.noreply.github.com>
2024-09-26 12:21:00 +0100

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -29,13 +34,24 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
             alt="cloudy"
           />
-          16°C
+
+          <span className="temperature">16</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 10%</li>
-            <li>Humidity: 73%</li>
-            <li>Wind: 8mph</li>
+            <li>
+              {" "}
+              <strong>Precipitation:</strong> 10%
+            </li>
+            <li>
+              {" "}
+              <strong>Humidity:</strong> 73%
+            </li>
+            <li>
+              {" "}
+              <strong>Wind:</strong> 8mph
+            </li>
           </ul>
         </div>
       </div>
