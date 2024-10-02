4a81d59ab24af1969691d2077acab57891d73a47
nina43 <161338284+nina43@users.noreply.github.com>
2024-10-02 12:59:20 +0100

    created new component to show real-time weather and day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,30 +1,35 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+  const [forecast, setForecast] = useState(null);
+  const [loaded, setLoaded] = useState(false);
   function handleResponse(response) {
     console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "1bf07a19dabb35aod3aet5596042cc7f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`;
+  function getForecast() {
+    let apiKey = "1bf07a19dabb35aod3aet5596042cc7f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="clear-sky-day" size={55} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecastTemperature-max">19°</span>
-            <span className="WeatherForecastTemperature-min">10°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    getForecast();
+    return null;
+  }
 }
